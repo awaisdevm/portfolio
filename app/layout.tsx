b6cb10214f8adf17f3b40408f72a647df9be988f
awais <mughal963@gmail.com>
2026-02-02 14:49:52 +0500

    meta data for SEO added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,33 @@ export default function RootLayout({
     <html lang="en" className="" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/fav-icons.svg" />
+        <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org/",
+      "@type": "Person",
+      "name": "Muhammad Awais",
+      "jobTitle": "Senior Software Engineer",
+      "url": "https://devawais.com",
+      "sameAs": [
+        "https://linkedin.com/in/awaisdevm",
+        "https://github.com/awaisdevm" 
+      ],
+      "knowsAbout": [
+        "Android Development",
+        "Mobile Application Development",
+        "iOS Development",
+        "Flutter",
+        "Kotlin",
+        "Dart",
+        "Jetpack Compose",
+        "Hilt",
+        
+      ]
+    }),
+  }}
+/>
       </head>
 
       <body className="bg-background text-foreground  transition-all  duration-500">
